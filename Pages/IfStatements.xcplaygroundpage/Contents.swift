979353fd5070f56c45c93fc6bb6febc3c9b3fe35
Adam Davidson <agdavidson91@gmail.com>
2025-04-21 14:50:37 -0400

    func list

--- Pages/IfStatements.xcplaygroundpage/Contents.swift
+++ Pages/IfStatements.xcplaygroundpage/Contents.swift
@@ -5,5 +5,12 @@ import Foundation
 let greeting = "Hello, Adam"
 let age = 34
 
+if age > 33 {
+    print(greeting)
+}
 
-
+if greeting == "hi" {
+    print(age)
+}else {
+    print("Do Not Be Afraid!")
+}
