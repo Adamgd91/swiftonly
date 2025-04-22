812fbba54b3ca0a106749fe819873aaa50651235
Adam Davidson <agdavidson91@gmail.com>
2025-04-21 21:04:47 -0400

    asd

--- Pages/BasicTypes.xcplaygroundpage/Contents.swift
+++ Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -3,3 +3,15 @@ import Foundation
 var greeting = "Hello, playground"
 var age = 23
 var underAged = true
+
+
+let apples = 3
+let groceryRequest = "Go get me Apples: "
+
+print(groceryRequest + String(apples))
+
+let bacon = 4
+let myFav = "I eat "
+
+let testing = "\(myFav) around \(bacon) every morning!"
+print(testing)
