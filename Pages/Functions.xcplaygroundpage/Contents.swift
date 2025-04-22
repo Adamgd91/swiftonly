ad4a512fbd95e3b44eebcc96958499e8ee0719fb
Adam Davidson <agdavidson91@gmail.com>
2025-04-22 07:24:58 -0400

    functions

--- Pages/Functions.xcplaygroundpage/Contents.swift
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -3,3 +3,17 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+func iosDev() {
+    print( "I will become an iOS Developer!")
+    oldToDrink(age: 33)
+}
+
+func oldToDrink(age: Double){
+    if age > 21 {
+        print("Old enough to drink!")
+    } else {
+        print("You can't dirnk! haha!")
+    }
+}
+
+iosDev()
